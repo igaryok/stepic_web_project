08d93b139e2dae2d2a2e56f87fd6229e0e0f9619
igaryok <igor.sobko@gmail.com>
2017-01-18 14:52:21 +0200

    new task

---
+++ web/hello.py
@@ -0,0 +1,13 @@
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    body = ""
+    for each in environ['QUERY_STRING']:
+        if each == '&':
+            body += "\n"
+        else:
+            body += each
+
+    start_response(status, headers)
+
+    return body
